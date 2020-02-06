5744feb073ebc3b20464a3a707388d2d35148e8d
AlexRecarte <60509746+AlexRecarte@users.noreply.github.com>
2020-02-06 11:41:35 +0100

    CalculadoraV1.2
    -Añadido Título
    -Algunos errores solucionados

--- Calculadora mejorada/calculadora.java
+++ Calculadora mejorada/calculadora.java
@@ -1,11 +1,14 @@
 import java.util.*;
 public class calculadora{
   public static void main(String[] args) {
+    clearScreen();
+    titulo();
+    esperar();
     menuPrincipal();
   }
   public static void menuPrincipal(){
     clearScreen();
-    System.out.print("¡Bienvenido a la Calculadora!\n\nMENU PRINCIPAL\n\n1. Calculadora\n2. Extension de calculadora\n3. Informacion adicional\n0. Salir\n\n(Elige la opcion deseada escribiendo en la consola el numero indicado)\n\n>");
+    System.out.print("Bienvenido a la Calculadora\n\nMENU PRINCIPAL\n\n1. Calculadora\n2. Extension de calculadora\n0. Salir\n\n(Elige la opcion deseada escribiendo en la consola el numero indicado)\n\n>");
     int eleccion = teclado.nextInt();
     switch(eleccion){
       case 1:
@@ -15,6 +18,7 @@ public class calculadora{
         extensionMenu();
         break;
       case 0:
+        System.exit(0);
         break;
       default:
         System.out.print("Introduce un numero valido");
@@ -126,8 +130,7 @@ public class calculadora{
           break;
         //SALIDA
         case 0:
-          String[] argumentos = new String [0];
-          main(argumentos);
+          menuPrincipal();
           break;
         default:
           System.out.print("Introduce un numero valido");
@@ -155,12 +158,12 @@ public class calculadora{
     int n = 0;
     String espacio = " ";
     int iaux = 0;
-    System.out.println ("\nIntroduce el numero que desea factorizar\n");
+    System.out.print("\nIntroduce el numero que desea factorizar\n\n>");
     n = teclado.nextInt();
     int l = n;
     System.out.print("\n");
-    String espacioauxiliar = "";
-    String espaciofinal= "";
+    String espacioauxiliar = "   ";
+    String espaciofinal= "   ";
     int naux = n;
     for (int i= 2 ; i<=n;)
     {
@@ -262,14 +265,14 @@ public class calculadora{
       switch(teclado.nextInt())
       {
         case 1:
-          System.out.print(factorizar() + "\n\nIntroduce cualquier cosa para continuar\n\n>");
+          System.out.print(factorizar() + "\n\nIntroduce cualquier digito para continuar\n\n>");
           teclado.next();
           extensionMenu();
           break;
         case 2:
-          System.out.println("\nIntroduce el primer numero\n\n>");
+          System.out.print("\nIntroduce el primer numero:\n\n>");
           int a = teclado.nextInt();
-          System.out.println("\nIntroduce el segundo numero\n\n>");
+          System.out.print("\nIntroduce el segundo numero:\n\n>");
           int b = teclado.nextInt();
           System.out.println("\nSolucion: " +mcd(a, b));
           esperar();
@@ -281,13 +284,15 @@ public class calculadora{
           extensionMenu();
           break;
         case 0:
-          String[] argumentos = new String [0];
-          main(argumentos);
+          menuPrincipal();
           break;
         default:
           System.out.println("\nIntroduce un numero valido\n\n");
           esperar();
           extensionMenu();
       }//fin switch
-  }//fin main
+  }//fin extensionMenu
+  public static void titulo(){
+    System.out.print("    .d8888b.           888                   888               888                          \n   d88P  Y88b          888                   888               888                          \n   888    888          888                   888               888                          \n   888         8888b.  888  .d8888b 888  888 888  8888b.   .d88888  .d88b.  888d888 8888b.  \n   888             88b 888 d88P     888  888 888      88b d88  888 d88  88b 888P        88b \n   888    888 .d888888 888 888      888  888 888 .d888888 888  888 888  888 888    .d888888 \n   Y88b  d88P 888  888 888 Y88b.    Y88b 888 888 888  888 Y88b 888 Y88..88P 888    888  888 \n     Y8888P    Y888888 888   Y8888P   Y88888 888  Y888888   Y88888   Y88P   888     Y888888 \n\n\n\n\n\n\n\n\n");
+  }//Fin titulo
 }//Fin class
